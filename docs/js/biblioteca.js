0c7db669f769d0c388242e3a59e4e005ff6f6beb
Assis-S-M <87490535+Assis-S-M@users.noreply.github.com>
2022-07-06 11:02:53 -0300

    Add files via upload

--- docs/js/biblioteca.js
+++ docs/js/biblioteca.js
@@ -5,12 +5,3 @@ menu.addEventListener("click", () => {
     
     navMenu.classList.toggle('disabled');
 });
-
-document.addEventListener("keypress", function(e) {
-    if(e.key === 'Enter') {
-    
-        var btn = document.querySelector("#submit");
-        
-        btn.click();
-    }
-});
