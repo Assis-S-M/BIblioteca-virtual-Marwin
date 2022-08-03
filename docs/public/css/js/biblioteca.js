ffde49f872e937d4835bc9e27d8c1c912bd180fb
Assis-S-M <87490535+Assis-S-M@users.noreply.github.com>
2022-08-03 08:33:36 -0300

    Delete docs/public/css/js directory

--- docs/public/css/js/biblioteca.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector("#divIcon");
-const navMenu = document.querySelector("ul");
-menu.addEventListener("click", () => {
-    
-    navMenu.classList.toggle('disabled');
-});
