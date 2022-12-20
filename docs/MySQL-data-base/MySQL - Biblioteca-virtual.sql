96b50253a7a6d5c82c402247a8529ea6475767c6
Assis-S-M <87490535+Assis-S-M@users.noreply.github.com>
2022-12-20 13:58:53 -0300

    Add files via upload

--- docs/MySQL-data-base/MySQL - Biblioteca-virtual.sql
+++ docs/MySQL-data-base/MySQL - Biblioteca-virtual.sql
@@ -1,30 +1,37 @@
-CREATE DATABASE  IF NOT EXISTS `biblioteca` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `biblioteca`;
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: biblioteca
--- ------------------------------------------------------
--- Server version	5.7.36
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 20-Dez-2022 às 16:55
+-- Versão do servidor: 5.7.36
+-- versão do PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `livros`
+-- Banco de dados: `biblioteca`
+--
+DROP DATABASE IF EXISTS `biblioteca`;
+CREATE DATABASE IF NOT EXISTS `biblioteca` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `biblioteca`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
 --
 
 DROP TABLE IF EXISTS `livros`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `livros` (
+CREATE TABLE IF NOT EXISTS `livros` (
   `nome do livro` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   `nome do autor` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
   `editora` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -33,26 +40,27 @@ CREATE TABLE `livros` (
   `aluno possuinte` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `livros`
+-- Extraindo dados da tabela `livros`
 --
 
-LOCK TABLES `livros` WRITE;
-/*!40000 ALTER TABLE `livros` DISABLE KEYS */;
-INSERT INTO `livros` VALUES ('Livro final','Autor final','Editora final','2022-07-20','Devolvido','Aluno final',1),('Teste final','Autor final','Editora final','2022-07-19','pendente','Aluno final',2),('Teste final','Autor final','Editora final','2022-07-19','pendente','Aluno final',3),('Teste final','Autor final','Editora final','2022-07-22','devolvido','Aluno final',4),('Teste final','Autor final','Editora final','2022-07-22','devolvido','Aluno final',5);
-/*!40000 ALTER TABLE `livros` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `livros` (`nome do livro`, `nome do autor`, `editora`, `data de devolucao`, `situacao atual`, `aluno possuinte`, `id`) VALUES
+('Livro final', 'Autor final', 'Editora final', '2022-12-08', 'devolvido', 'Aluno modificado', 1),
+('Teste final', 'Autor final', 'Editora final', '2022-07-19', 'pendente', 'Aluno final', 2),
+('Teste final', 'Autor final', 'Editora final', '2022-07-19', 'pendente', 'Aluno final', 3),
+('Teste final', 'Autor final', 'Editora final', '2022-07-22', 'devolvido', 'Aluno final', 4),
+('Teste final', 'Autor final', 'Editora final', '2022-07-22', 'devolvido', 'Aluno final', 5),
+('gfiaufbvsvgs', 'ssssss', 'sssssss', '2022-12-06', 'devolvido', 'dfddddd', 6),
+('eeeee', 'eeee', 'eeee', '2022-12-09', 'pendente', 'eeeee', 7),
+('eeeeeee', 'eee', 'eeeee', '2022-12-09', 'pendente', 'eeeee', 8),
+('eeeeeee', 'eeeeee', 'eeee', '2022-12-01', 'pendente', 'eeee', 9),
+('uuuuuuuuuuuuuuuuuuuuuuuuuuuuu', 'uuuuuuuuuuuuuuuuuuuuuuuuuu', 'uuuuuuuuuuuuuuuuuuuuuu', '2022-12-08', 'pendente', 'uuuuuuuuuuuu', 10),
+('teste de pesquisa', 'eeee', 'eeee', '2023-01-04', 'devolvido', 'eeee', 11),
+('aaaaaaa', 'aaaaaaaa', 'aaaaaaa', '2022-12-01', 'pendente', 'teste pesquisa aluno', 12);
+COMMIT;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-07-06 10:42:07
